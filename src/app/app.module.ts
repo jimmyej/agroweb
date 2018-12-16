122d37dde83294dc910fe3ceb7f2477b7c71f30c
Jimmy Sanchez <jimmyej23@gmail.com>
2018-12-15 19:16:22 -0500

    billing module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { TabViewModule } from 'primeng/tabview';
 
 import { AppComponent } from './app.component';
+import { BillingComponent } from './billing/billing.component';
+
+import { InvoiceListComponent } from './billing/invoices/invoice-list/invoice-list.component';
+import { InquiryListComponent } from './billing/inquiries/inquiry-list/inquiry-list.component';
+import { PaymentListComponent } from './billing/payments/payment-list/payment-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BillingComponent,
+    InvoiceListComponent,
+    InquiryListComponent,
+    PaymentListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TabViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
